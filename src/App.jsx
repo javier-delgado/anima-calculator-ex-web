904db24ea95143660330feaeccabe51feaaea50b
Javier Delgado <javierdelgadogm@gmail.com>
2019-06-15 23:20:14 -0300

    Added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import './App.scss';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/styles';
+import ReactGA from 'react-ga';
 import Container from '@material-ui/core/Container';
 import CharacterList from './components/characterList/characterList.connector';
 import theme from './materialTheme';
+
+ReactGA.initialize(process.env.REACT_APP_ANAYLTICS_ID);
+ReactGA.pageview('/');
 
 function App() {
   return (
